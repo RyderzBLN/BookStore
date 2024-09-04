da5fd73992da6ab1cca9dcff9927bb5a4245c279
Murat <mcatili@web.de>
2024-09-04 20:00:46 +0200

    finish with complete comment

--- script.js
+++ script.js
@@ -4,41 +4,45 @@ let books = [
       "preis": 12.99,
       "autor": "Ernest Hemingway",
       "erscheinungsjahr": 1952,
-      "genre": "Klassiker"
+      "genre": "Klassiker",
+      "like": 234
     },
     {
       "buchname": "1984",
       "preis": 10.49,
       "autor": "George Orwell",
       "erscheinungsjahr": 1949,
-      "genre": "Dystopie"
+      "genre": "Dystopie",
+      "like": 123
     },
     {
       "buchname": "Die unendliche Geschichte",
       "preis": 15.99,
       "autor": "Michael Ende",
       "erscheinungsjahr": 1979,
-      "genre": "Fantasy"
+      "genre": "Fantasy",
+      "like": 23
     },
     {
       "buchname": "Der Name des Windes",
       "preis": 18.99,
       "autor": "Patrick Rothfuss",
       "erscheinungsjahr": 2007,
-      "genre": "Fantasy"
+      "genre": "Fantasy",
+      "like": 92
     },
     {
       "buchname": "Die große Reise",
       "preis": 8.99,
       "autor": "J.K. Rowling",
       "erscheinungsjahr": 2020,
-      "genre": "Abenteuer"
+      "genre": "Abenteuer",
+      "like": 21
     }
   ];
   
 
-
-function bookTemplate(book){
+function bookTemplate(book, index){
 
     return `<div class="book-card">
     <div class="headline"><h3>${book.buchname}</h3></div>
@@ -46,7 +50,7 @@ function bookTemplate(book){
 
     <div class="price-and-like">
         <div class="price">${book.preis}$</div>
-        <div class="like-button">Herz</div>
+        <div class="like-button"><div onclick="clickHeart()" id="heart-button" class="bg-red"><img class="herz" src="./src/img/hearth.svg"></div><span>${book.like}</span></div>
     </div>
     <div class="book-info">
         <div class="autor"><p>Autor:</p><p>${book.autor}</p></div>
@@ -55,14 +59,13 @@ function bookTemplate(book){
     </div>
 
     <div class="comments-heading"><h4>Kommentare:</h4></div>
-    <div id="comment" class="comment-section">        
+    <div id="comment-${index}" class="comment-section">        
     </div>
  <div class="input-section">
-    <input id="inputfield" type="text" placeholder="Kommentar eingeben">
-    <button onclick="addComment()">send</button>
+    <input id="inputfield-${index}" type="text" placeholder="Kommentar eingeben">
+    <button onclick="addComment(${index})">send</button>
 </div>`    
 }
-
 
 
 function renderBooks(){
@@ -70,24 +73,24 @@ function renderBooks(){
     contentRef.innerHTML = ''; 
 
     for (let i = 0; i < books.length; i++) {
-       contentRef.innerHTML += bookTemplate(books[i]);
+       contentRef.innerHTML += bookTemplate(books[i], i);
         
     }
 }
 
 
-function addComment(){
-
-  let inputRef = document.getElementById("inputfield");  
-  let contentRef = document.getElementById("comment");
+function addComment(index){
+  let inputRef = document.getElementById(`inputfield-${index}`);  
+  let contentRef = document.getElementById(`comment-${index}`);
   let noteInput = inputRef.value;
+  
   contentRef.innerHTML += commentTemplate(noteInput);
   inputRef.value = "";
-
-
 }
 
-function commentTemplate(noteInput){
-  
+
+function commentTemplate(noteInput){ 
   return `<p>User32: "${noteInput}"</p>`
 }
+
+renderBooks();
